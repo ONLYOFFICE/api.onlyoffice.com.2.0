dded5aea54626fc3f38b59ca714fad115806c60a
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-20 16:55:18 +0400

    up makefile

--- makefile.ts
+++ makefile.ts
@@ -4,32 +4,23 @@ import {join} from "node:path"
 import {argv} from "node:process"
 import {URL, fileURLToPath} from "node:url"
 import sade from "sade"
-import * as resources from "./resources/makefile.ts"
-import * as iconJS from "./ui/icon/makefile.ts"
-import * as logoJS from "./ui/logo/makefile.ts"
 
 main()
 
 function main(): void {
-  sade("./makefile.js")
+  sade("./makefile.ts")
     .command("build")
-    .action(build)
+    .action(async () => {
+      await run("build")
+    })
     .command("test")
-    .action(test)
+    .action(async () => {
+      await run("test")
+    })
     .parse(argv)
 }
 
-async function build(): Promise<void> {
-  await Promise.all([
-    resources.build(),
-
-    // todo: move to ui/kit-js.
-    iconJS.build(),
-    logoJS.build()
-  ])
-}
-
-async function test(): Promise<void> {
+async function run(cmd: string): Promise<void> {
   const cd = currentDir()
   const pd = packagesDir(cd)
   const pc = await readdir(pd)
@@ -38,14 +29,11 @@ async function test(): Promise<void> {
     const f = packageJSON(d)
     const c = await readFile(f, "utf8")
     const j = JSON.parse(c)
-    if (!j.scripts) {
-      continue
-    }
-    if (!j.scripts.test) {
+    if (!(j.scripts && j.scripts[cmd])) {
       continue
     }
     await new Promise((res, rej) => {
-      const s = spawn("pnpm", ["test"], {cwd: d, stdio: "inherit"})
+      const s = spawn("pnpm", [cmd], {cwd: d, stdio: "inherit"})
       s.on("close", res)
       s.on("error", rej)
     })
