623fc452ffeca967f2849a3b027b87b8a5028653
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-20 16:44:37 +0400

    add resource package

---
+++ packages/resource/lib/main.test.ts
@@ -0,0 +1,37 @@
+import {StringWritable} from "@onlyoffice/stream-string"
+import {is, match, unreachable} from "uvu/assert"
+import {test} from "uvu"
+import {rawURL, readURL, declarationFile, indexFile} from "./main.ts"
+
+test("creates a raw github url", () => {
+  const u = rawURL("o", "rp", "rf", "p")
+  is(u, "https://raw.githubusercontent.com/o/rp/rf/p")
+})
+
+test("reads a url content into a writable stream", async () => {
+  const w = new StringWritable()
+  await readURL(w, "https://www.google.com/")
+  match(w.buf, /<!doctype html>/)
+})
+
+test("throws an error if a status code is not 200", async () => {
+  const w = new StringWritable()
+  try {
+    await readURL(w, "https://google.com/")
+    unreachable(`Expected an error, got ${w.buf}`)
+  } catch (e) {
+    is(e instanceof Error && e.message, "Bad status code: 301 Moved Permanently")
+  }
+})
+
+test("creates a declaration file name", () => {
+  const n = declarationFile("n")
+  is(n, "n.declaration.json")
+})
+
+test("creates an index file name", () => {
+  const n = indexFile("n")
+  is(n, "n.index.json")
+})
+
+test.run()
---
+++ packages/resource/lib/main.ts
@@ -0,0 +1,31 @@
+import {get} from "node:https"
+import type {Writable} from "node:stream"
+
+export function rawURL(o: string, rp: string, rf: string, p: string): string {
+  const u = new URL(`/${o}/${rp}/${rf}/${p}`, "https://raw.githubusercontent.com/")
+  return u.toString()
+}
+
+export function readURL(w: Writable, u: string): Promise<void> {
+  return new Promise((res, rej) => {
+    get(u, (r) => {
+      if (r.statusCode !== 200) {
+        r.destroy()
+        const e = new Error(`Bad status code: ${r.statusCode} ${r.statusMessage}`)
+        rej(e)
+        return
+      }
+      r.pipe(w)
+      w.on("error", rej)
+      w.on("finish", res)
+    })
+  })
+}
+
+export function declarationFile(n: string): string {
+  return `${n}.declaration.json`
+}
+
+export function indexFile(n: string): string {
+  return `${n}.index.json`
+}
