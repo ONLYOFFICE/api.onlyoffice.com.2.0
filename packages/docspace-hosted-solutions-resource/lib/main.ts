9371d21dccc1890fa10acf2a469025e987749336
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-21 16:28:36 +0400

    reuse generic functions from the openapi-resource package

--- packages/docspace-hosted-solutions-resource/lib/main.ts
+++ packages/docspace-hosted-solutions-resource/lib/main.ts
@@ -1,26 +1,12 @@
-import {mkdir, mkdtemp, rm, rmdir, writeFile} from "node:fs/promises"
-import type {WriteStream} from "node:fs"
+import {mkdir, rm} from "node:fs/promises"
 import {createWriteStream, existsSync} from "node:fs"
-import {tmpdir} from "node:os"
-import {dirname, join} from "node:path"
-import type {Writable} from "node:stream"
+import {join} from "node:path"
 import {URL, fileURLToPath} from "node:url"
 import {Console} from "@onlyoffice/console"
-import {hasJQ, jq} from "@onlyoffice/jq"
-import {Cache, ProcessComponent, ProcessPath, ProcessRequest} from "@onlyoffice/openapi-declaration"
-import {PickComponent, PickPath} from "@onlyoffice/openapi-resource"
-import {relative} from "@onlyoffice/path"
+import {Cache} from "@onlyoffice/openapi-declaration"
+import {writeComponent, writeDeclaration, writeEntrypoint} from "@onlyoffice/openapi-resource"
 import {componentBasename, declarationBasename, rawURL, readURL, resourceBasename} from "@onlyoffice/resource"
-import {resource} from "@onlyoffice/service-resource"
-import {UnstreamObject, makeObject} from "@onlyoffice/stream-json"
-import {StringReadable, StringWritable} from "@onlyoffice/stream-string"
-import MultiStream from "multistream"
-import Chain from "stream-chain"
-import StreamArray from "stream-json/streamers/StreamArray.js"
-import StreamObject from "stream-json/streamers/StreamObject.js"
-import Disassembler from "stream-json/Disassembler.js"
-import Parser from "stream-json/Parser.js"
-import Stringer from "stream-json/Stringer.js"
+import {StringWritable} from "@onlyoffice/stream-string"
 import pack from "../package.json" with {type: "json"}
 
 const config = [
@@ -81,143 +67,6 @@ async function main(): Promise<void> {
   }
 
   console.log("Finish building")
-}
-
-async function writeDeclaration(ch: Cache, rw: StringWritable, dw: Writable): Promise<void> {
-  let from: StringWritable
-  let to = new StringWritable()
-  await new Promise((res, rej) => {
-    const c = new Chain([
-      rw.toReadable(),
-      new Parser(),
-      new PickPath(),
-      new StreamObject(),
-      new ProcessPath(ch),
-      new Disassembler(),
-      new Stringer({makeArray: true}),
-      to
-    ])
-    c.on("close", res)
-    c.on("error", rej)
-  })
-
-  from = to
-  to = new StringWritable()
-  await new Promise((res, rej) => {
-    const c = new Chain([
-      from.toReadable(),
-      new Parser(),
-      new StreamArray(),
-      new ProcessRequest(ch),
-      new Disassembler(),
-      new Stringer({makeArray: true}),
-      to
-    ])
-    c.on("close", res)
-    c.on("error", rej)
-  })
-
-  from = to
-  to = new StringWritable()
-  await new Promise((res, rej) => {
-    const c = new Chain([
-      new MultiStream([
-        // todo: see how it handles in the jsdoc
-        new StringReadable(JSON.stringify(Object.values(ch.groups))),
-        from.toReadable()
-      ]),
-      new Parser({jsonStreaming: true}),
-      new StreamArray(),
-      (ch: {value: unknown}): unknown => {
-        return ch.value
-      },
-      new Disassembler(),
-      new Stringer({makeArray: true}),
-      to
-    ])
-    c.on("close", res)
-    c.on("error", rej)
-  })
-
-  if (!await hasJQ()) {
-    dw.write(to.buf)
-  } else {
-    const td = await mkdtemp(`${tempDir()}-`)
-    const tf = join(td, "d")
-    await writeFile(tf, to.buf)
-    await jq(dw, [".", tf])
-    await rm(tf)
-    await rmdir(td)
-  }
-}
-
-async function writeComponent(ch: Cache, rw: StringWritable, cw: Writable): Promise<void> {
-  let from: StringWritable
-  let to = new StringWritable()
-
-  const ks: ConstructorParameters<typeof PickComponent>[0][] =
-  ["schemas", "responses"]
-  for (const k of ks) {
-    // eslint-disable-next-line no-loop-func
-    await new Promise((res, rej) => {
-      const c = new Chain([
-        rw.toReadable(),
-        new Parser(),
-        new PickComponent(k),
-        new StreamObject(),
-        new ProcessComponent(ch, k),
-        new UnstreamObject(),
-        makeObject(),
-        new Stringer(),
-        to
-      ])
-      c.on("close", res)
-      c.on("error", rej)
-    })
-    to = new StringWritable(to.buf)
-  }
-
-  // eslint-disable-next-line prefer-const
-  from = to
-  to = new StringWritable()
-  await new Promise((res, rej) => {
-    const c = new Chain([
-      from.toReadable(),
-      new Parser({jsonStreaming: true}),
-      new StreamObject(),
-      new UnstreamObject(),
-      makeObject(),
-      new Stringer(),
-      to
-    ])
-    c.on("close", res)
-    c.on("error", rej)
-  })
-
-  if (!await hasJQ()) {
-    cw.write(to.buf)
-  } else {
-    const td = await mkdtemp(`${tempDir()}-`)
-    const tf = join(td, "c")
-    await writeFile(tf, to.buf)
-    await jq(cw, [".", tf])
-    await rm(tf)
-    await rmdir(td)
-  }
-}
-
-async function writeEntrypoint(ew: WriteStream, df: string, cf: string): Promise<void> {
-  const ef = String(ew.path)
-  const ed = dirname(ef)
-  df = relative(ed, df)
-  cf = relative(ed, cf)
-  const c = await resource(df, cf)
-  ew.write(c)
-}
-
-function tempDir(): string {
-  const n = pack.name.replace("/", "+")
-  return join(tmpdir(), n)
 }
 
 function rootDir(): string {
