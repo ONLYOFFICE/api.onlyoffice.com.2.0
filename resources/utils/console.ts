268dc1333c84e7275c8fe34c7b1540f026654da8
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-22 08:50:12 +0400

    remove the old resources package

--- resources/utils/console.ts
+++
@@ -1,5 +0,0 @@
-import process from "node:process"
-import {Console} from "@onlyoffice/console"
-import pack from "../package.json" with {type: "json"}
-
-export const console = new Console(pack.name, process.stdout, process.stderr)
