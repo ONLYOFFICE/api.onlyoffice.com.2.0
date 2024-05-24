bfd1dc8824bed5c83240d722ac6843e9c9a95713
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-24 17:16:32 +0400

    add elements to the site deps

--- site/index.d.ts
+++ site/index.d.ts
@@ -1,1 +1,5 @@
+/// <reference types="@onlyoffice/document-builder-html-element" />
+/// <reference types="@onlyoffice/document-editor-html-element" />
+/// <reference types="@onlyoffice/document-editor-mirror-html-element" />
+/// <reference types="@onlyoffice/document-editor-playground-html-element" />
 /// <reference types="@onlyoffice/xml-types" />
