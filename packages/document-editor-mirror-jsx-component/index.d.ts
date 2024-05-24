07c3b0aca7f888b2660c296c650845ed19bed082
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-24 16:16:33 +0400

    remove document-editor-mirror-jsx-component package

--- packages/document-editor-mirror-jsx-component/index.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference types="@onlyoffice/document-editor-mirror-html-element" />
--- packages/document-editor-mirror-jsx-component/lib/main.tsx
+++
@@ -1,8 +0,0 @@
-import type {JSX} from "preact"
-import {h} from "preact"
-
-export function DocumentEditorMirror(
-  {children, ...args}: JSX.IntrinsicElements["document-editor-mirror"]
-): JSX.Element {
-  return <document-editor-mirror {...args}>{children}</document-editor-mirror>
-}
