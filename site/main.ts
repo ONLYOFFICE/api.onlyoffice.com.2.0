4e582bfb13ff412b2c398d7e548d7c5229e584bd
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-05-29 17:24:25 +0400

    structure site js entrypoint

--- site/main.ts
+++ site/main.ts
@@ -1,9 +1,22 @@
-import "@onlyoffice/documentation-ui-kit/kit.client.ts"
-import "./components/tree/tree.client.ts"
-
-// todo: move to ui
-import "./components/clipboard-copy/clipboard-copy.client.ts"
-
-import "@onlyoffice/document-editor-html-element"
 import "@onlyoffice/document-builder-html-element"
+import "@onlyoffice/document-editor-html-element"
+import "@onlyoffice/document-editor-mirror-html-element"
+import "@onlyoffice/document-editor-playground-html-element"
+import "@onlyoffice/documentation-ui-kit/kit.client.ts"
+import "@onlyoffice/site-playground/client.ts"
+import "./components/tree/tree.client.ts"
+import "./components/clipboard-copy/clipboard-copy.client.ts"
 import "./components/document-builder-container/element.ts"
+import {Client} from "@onlyoffice/server-client"
+
+function main(): void {
+  const c = new Client()
+  c.baseURL = "http://localhost:4001/"
+
+  const sp = document.querySelector("site-playground")
+  if (sp) {
+    sp.client = c
+  }
+}
+
+document.addEventListener("DOMContentLoaded", main)
