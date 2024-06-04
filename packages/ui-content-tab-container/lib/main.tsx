d41fc06fe935bd2340461ef40b195666851eb235
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-04 15:21:06 +0400

    rename to ui-content-tab-container

---
+++ packages/ui-content-tab-container/lib/main.tsx
@@ -0,0 +1,37 @@
+import type {JSX} from "preact"
+import {h} from "preact"
+
+export interface ContentTabContainerParameters {
+  children: any
+}
+
+export function ContentTabContainer({children}: ContentTabContainerParameters): JSX.Element {
+  return <tab-container>{children}</tab-container>
+}
+
+export interface ContentTabListParameters {
+  children: any
+  label: string
+}
+
+export function ContentTabList({children, label}: ContentTabListParameters): JSX.Element {
+  return <div role="tablist" aria-label={label}>{children}</div>
+}
+
+export interface ContentTabParameters {
+  children: any
+  id: string
+}
+
+export function ContentTab({children, id}: ContentTabParameters): JSX.Element {
+  return <div id={id} role="tab">{children}</div>
+}
+
+export interface ContentTabPanelParameters {
+  by: string
+  children: any
+}
+
+export function ContentTabPanel({by, children}: ContentTabPanelParameters): JSX.Element {
+  return <div role="tabpanel" aria-labelledby={by}>{children}</div>
+}
