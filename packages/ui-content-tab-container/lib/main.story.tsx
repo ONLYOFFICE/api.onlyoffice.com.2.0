3b661e0252209b1e9ee5d3312ec24601122868da
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-04 19:08:53 +0400

    add story for the ui-content-tab-container

---
+++ packages/ui-content-tab-container/lib/main.story.tsx
@@ -0,0 +1,37 @@
+import "./client.ts"
+import colors from "@onlyoffice/ui-colors/colors.css?inline"
+import content from "@onlyoffice/ui-content/content.css?inline"
+import {Content} from "@onlyoffice/ui-content"
+import type {JSX} from "preact"
+import {Fragment, h} from "preact"
+import css from "./main.css?inline"
+import {ContentTab, ContentTabContainer, ContentTabList, ContentTabPanel} from "./main.tsx"
+
+const styles: string[] = [colors, content, css]
+
+export default {
+  title: "UI/Content Tab Container",
+  decorators: [
+    (Story: () => JSX.Element): JSX.Element => <>
+      {styles.map((s) => <style key={s} dangerouslySetInnerHTML={{__html: s}} />)}
+      <Story />
+    </>
+  ]
+}
+
+export function Composition(): JSX.Element {
+  return <Content>
+    <ContentTabContainer>
+      <ContentTabList label="Group of Tabs">
+        <ContentTab id="first">First Tab</ContentTab>
+        <ContentTab id="second">Second Tab</ContentTab>
+      </ContentTabList>
+      <ContentTabPanel by="first">
+        <p>First Tab Content</p>
+      </ContentTabPanel>
+      <ContentTabPanel by="second">
+        <p>Second Tab Content</p>
+      </ContentTabPanel>
+    </ContentTabContainer>
+  </Content>
+}
