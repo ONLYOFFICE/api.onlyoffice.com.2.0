ef09f704eb336e7824c3cdf502f6633d0a32d980
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-04 18:50:50 +0400

    make the children parameter less strictive

--- packages/ui-content/lib/main.tsx
+++ packages/ui-content/lib/main.tsx
@@ -2,7 +2,7 @@ import type {JSX} from "preact"
 import {h} from "preact"
 
 export interface ContentParameters {
-  children: any
+  children?: any
 }
 
 export function Content({children}: ContentParameters): JSX.Element {
