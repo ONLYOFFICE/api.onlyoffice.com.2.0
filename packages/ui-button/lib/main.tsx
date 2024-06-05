bbc39762b7c65f54232334d33b2675ed4c41fe7e
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-05 12:07:11 +0400

    add more props for the ui-button

--- packages/ui-button/lib/main.tsx
+++ packages/ui-button/lib/main.tsx
@@ -1,19 +1,21 @@
 import {clsx} from "clsx"
+import type {HTMLAttributes} from "preact/compat"
 import type {JSX} from "preact"
 import {h} from "preact"
 
-export interface ButtonParameters {
+export interface ButtonParameters extends HTMLAttributes<HTMLButtonElement> {
   children?: any
+  type?: "button" | "submit"
   variant?: "accent" | "neutral"
 }
 
-export function Button({children, variant}: ButtonParameters): JSX.Element {
+export function Button({children, type = "button", variant, ...props}: ButtonParameters): JSX.Element {
   const cv = ["button"]
   if (variant !== undefined) {
     const c = vc(variant)
     cv.push(c)
   }
-  return <button class={clsx(cv)} type="button">{children}</button>
+  return <button class={clsx(cv)} type={type} {...props}>{children}</button>
 }
 
 function vc(v: Exclude<ButtonParameters["variant"], undefined>): string {
