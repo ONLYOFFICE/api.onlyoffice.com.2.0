60290b5e23af5fba6edf678fde387e3f47493e9d
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-11 15:04:24 +0400

    use site-config-fixtures in site-document-editor-playground

--- packages/site-document-editor-playground/lib/main.story.tsx
+++ packages/site-document-editor-playground/lib/main.story.tsx
@@ -1,16 +1,7 @@
 import "@onlyoffice/ui-select/client.ts"
 import "./client.ts"
 import {Client} from "@onlyoffice/server-client"
-import {
-  BooleanType,
-  EnumType,
-  FunctionType,
-  LiteralType,
-  PlaygroundConfig,
-  PropertyConfig,
-  StringType,
-  TabConfig
-} from "@onlyoffice/site-config"
+import {ConfigFixture} from "@onlyoffice/site-config-fixtures"
 import button from "@onlyoffice/ui-button/main.css?inline"
 import codeEditor from "@onlyoffice/ui-code-editor/main.css?inline"
 import codeListing from "@onlyoffice/ui-code-listing/main.css?inline"
@@ -54,80 +45,8 @@ export function Composition(): JSX.Element {
     setup()
   }, [])
 
-  const config = new PlaygroundConfig()
-
-  config.documentEditor.documentServerUrl = "http://0.0.0.0:3000/"
-
-  let pr = new PropertyConfig()
-  pr.path = "documentType"
-  pr.tab = "base"
-  pr.href = "#documentType"
-  let et = new EnumType()
-  let lt = new LiteralType()
-  lt.base = new StringType()
-  lt.const = "word"
-  et.cases.push(lt)
-  lt = new LiteralType()
-  lt.base = new StringType()
-  lt.const = "cell"
-  et.cases.push(lt)
-  pr.type = et
-  pr.default = "word"
-  config.documentEditor.config.push(pr)
-
-  pr = new PropertyConfig()
-  pr.path = "document.fileType"
-  pr.tab = "document"
-  pr.href = "#fileType"
-  et = new EnumType()
-  lt = new LiteralType()
-  lt.base = new StringType()
-  lt.const = "docx"
-  et.cases.push(lt)
-  lt = new LiteralType()
-  lt.base = new StringType()
-  lt.const = "xlsx"
-  et.cases.push(lt)
-  pr.type = et
-  pr.default = "docx"
-  config.documentEditor.config.push(pr)
-
-  pr = new PropertyConfig()
-  pr.path = "editorConfig.customization.compactHeader"
-  pr.tab = "customization"
-  pr.href = "#compactHeader"
-  pr.type = new BooleanType()
-  pr.default = false
-  config.documentEditor.config.push(pr)
-
-  pr = new PropertyConfig()
-  pr.path = "events.onAppReady"
-  pr.tab = "events"
-  pr.href = "#onAppReady"
-  pr.type = new FunctionType()
-  config.documentEditor.config.push(pr)
-
-  let ta = new TabConfig()
-  ta.id = "base"
-  ta.label = "Base"
-  config.tabs.push(ta)
-
-  ta = new TabConfig()
-  ta.id = "document"
-  ta.label = "Document"
-  config.tabs.push(ta)
-
-  ta = new TabConfig()
-  ta.id = "customization"
-  ta.label = "Editor Config: Customization"
-  config.tabs.push(ta)
-
-  ta = new TabConfig()
-  ta.id = "events"
-  ta.label = "Events"
-  config.tabs.push(ta)
-
-  return <SiteDocumentEditorPlayground config={config} />
+  const c = new ConfigFixture()
+  return <SiteDocumentEditorPlayground config={c.playground} />
 }
 
 async function setup(): Promise<void> {
