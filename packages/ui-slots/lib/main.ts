42ebb64c5ec82a4acf6975ea1b0316e9ebc1f9d4
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-13 16:14:29 +0400

    support for an array in the slots

--- packages/ui-slots/lib/main.ts
+++ packages/ui-slots/lib/main.ts
@@ -3,28 +3,49 @@ import {isValidElement, toChildArray} from "preact"
 // todo: refactor it.
 // https://github.com/primer/react/blob/ea44386e4d7afae7bfd07a679604baaecca55965/packages/react/src/hooks/useSlots.ts#L35
 export function useSlots<C extends Record<string, any>>(children: any, config: C): [C, any] {
-  const n = {}
-  const f = []
+  const sl = {}
+  const ou = []
 
   const ks = Object.keys(config)
   const vs = Object.values(config)
+  const cs = toChildArray(children)
 
-  for (const c of toChildArray(children)) {
-    if (!isValidElement(c)) {
-      f.push(c)
+  for (const ch of cs) {
+    if (!isValidElement(ch)) {
+      ou.push(ch)
       continue
     }
-    const i = vs.indexOf(c.type)
-    if (i === -1) {
-      f.push(c)
+
+    let ci = -1
+
+    for (const ta of vs) {
+      let co = ta
+      if (Array.isArray(ta)) {
+        [co] = ta
+      }
+      if (ch.type === co || ch.type === co.type) {
+        ci = vs.indexOf(ta)
+        break
+      }
+    }
+
+    if (ci === -1) {
+      ou.push(ch)
       continue
     }
-    const k = ks[i]
-    if (n[k]) {
+
+    const sk = ks[ci]
+
+    if (sl[sk]) {
+      if (!Array.isArray(sl[sk])) {
+        sl[sk] = [sl[sk]]
+      }
+      sl[sk].push(ch)
       continue
     }
-    n[k] = c
+
+    sl[sk] = ch
   }
 
-  return [n, f]
+  return [sl, ou]
 }
