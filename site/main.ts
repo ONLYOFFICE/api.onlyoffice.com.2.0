2d5c394e1dfa35076d6f1d77875616688b9f0985
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-06-14 15:35:02 +0400

    add new packages to site

--- site/main.ts
+++ site/main.ts
@@ -4,18 +4,25 @@ document.addEventListener("sitedocumenteditorplaygrounderror", onError)
 document.addEventListener("DOMContentLoaded", main)
 
 import "@onlyoffice/combobox-container-html-element"
+import "@onlyoffice/radiogroup-container-html-element"
+
 import "@onlyoffice/document-builder-html-element"
 import "@onlyoffice/document-editor-config-html-element"
 import "@onlyoffice/document-editor-html-element"
 import "@onlyoffice/document-editor-mirror-html-element"
+
 import "@onlyoffice/ui-select/client.ts"
 import "@onlyoffice/ui-content-tab-container/client.ts"
 import "@onlyoffice/ui-code-listing/client.ts"
 import "@onlyoffice/documentation-ui-kit/kit.client.ts"
+
+import "@onlyoffice/site-theme-switcher/client.ts"
 import "@onlyoffice/site-document-editor-playground/client.ts"
+
 import "./components/tree/tree.client.ts"
 import "./components/clipboard-copy/clipboard-copy.client.ts"
 import "./components/document-builder-container/element.ts"
+
 import {Client} from "@onlyoffice/server-client"
 
 async function main(): Promise<void> {
