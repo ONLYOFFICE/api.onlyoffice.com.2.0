8383ab490ac5c2450574c3a28c80c63e0333dc0f
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-10-09 12:40:52 +0400

    rename components of the site-service package

--- packages/site-service/lib/main.story.tsx
+++ packages/site-service/lib/main.story.tsx
@@ -4,15 +4,15 @@
 import {type Meta, type StoryObj} from "@storybook/preact"
 import {Fragment, h} from "preact"
 import {
-  ServiceDeclaration,
-  ServiceDeclarationDescription,
-  ServiceDeclarationHeading,
-  type ServiceDeclarationProperties,
-  ServiceDeclarationSyntaxHighlight,
+  Service,
+  ServiceDescription,
+  ServiceHeading,
+  type ServiceProperties,
+  ServiceSyntaxHighlight,
 } from "./main.tsx"
 
 export default {
-  title: "Site / Service Declaration",
+  title: "Site / Service",
 } satisfies Meta
 
 export const Default: StoryObj = {
@@ -26,11 +26,11 @@ export const Default: StoryObj = {
   },
   render() {
     // @ts-ignore todo
-    return <ServiceDeclaration declaration={{}} />
+    return <Service declaration={{}} />
   },
 }
 
-export const Playground: StoryObj<ServiceDeclarationProperties> = {
+export const Playground: StoryObj<ServiceProperties> = {
   parameters: {
     actions: {
       disable: true,
@@ -40,40 +40,40 @@ export const Playground: StoryObj<ServiceDeclarationProperties> = {
     // todo
   },
   render(p) {
-    return <ServiceDeclaration {...p}>
-      <ServiceDeclarationHeading for="Authorization">
+    return <Service {...p}>
+      <ServiceHeading for="Authorization">
         <h3>Authorization</h3>
-      </ServiceDeclarationHeading>
-      <ServiceDeclarationHeading for="Body">
+      </ServiceHeading>
+      <ServiceHeading for="Body">
         <h3>Body</h3>
-      </ServiceDeclarationHeading>
-      <ServiceDeclarationHeading for="Cookies">
+      </ServiceHeading>
+      <ServiceHeading for="Cookies">
         <h3>Cookies</h3>
-      </ServiceDeclarationHeading>
-      <ServiceDeclarationHeading for="Examples">
+      </ServiceHeading>
+      <ServiceHeading for="Examples">
         <h3>Examples</h3>
-      </ServiceDeclarationHeading>
-      <ServiceDeclarationHeading for="Headers">
+      </ServiceHeading>
+      <ServiceHeading for="Headers">
         <h3>Headers</h3>
-      </ServiceDeclarationHeading>
-      <ServiceDeclarationHeading for="Path">
+      </ServiceHeading>
+      <ServiceHeading for="Path">
         <h3>Path</h3>
-      </ServiceDeclarationHeading>
-      <ServiceDeclarationHeading for="Query">
+      </ServiceHeading>
+      <ServiceHeading for="Query">
         <h3>Query</h3>
-      </ServiceDeclarationHeading>
-      <ServiceDeclarationHeading for="Request">
+      </ServiceHeading>
+      <ServiceHeading for="Request">
         <h2>Request</h2>
-      </ServiceDeclarationHeading>
-      <ServiceDeclarationHeading for="Responses">
+      </ServiceHeading>
+      <ServiceHeading for="Responses">
         <h2>Responses</h2>
-      </ServiceDeclarationHeading>
-      <ServiceDeclarationDescription>
+      </ServiceHeading>
+      <ServiceDescription>
         {(p) => <p>{p.children}</p>}
-      </ServiceDeclarationDescription>
-      <ServiceDeclarationSyntaxHighlight>
+      </ServiceDescription>
+      <ServiceSyntaxHighlight>
         {(p) => <>{p.children}</>}
-      </ServiceDeclarationSyntaxHighlight>
-    </ServiceDeclaration>
+      </ServiceSyntaxHighlight>
+    </Service>
   },
 }
