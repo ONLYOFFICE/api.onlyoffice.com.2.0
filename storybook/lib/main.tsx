0c90804e3e4557ed910b384d1e1b76df3a2c3f10
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-10-10 09:45:52 +0400

    add storybook/addon-a11y

--- storybook/lib/main.tsx
+++ storybook/lib/main.tsx
@@ -9,6 +9,7 @@ export default {
   addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-storysource",
+    "@storybook/addon-a11y",
     "@storybook/addon-viewport",
   ],
   stories: [
