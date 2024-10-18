7f344f5da32ba647fbb37384b0e6f1a9923e581c
ZEROM22 <alexander.salyakhov@onlyoffice.com>
2024-10-18 17:32:17 +0400

    update jsdoc fixtures

--- packages/jsdoc-resource-fixtures/fixtures/resource.js
+++ packages/jsdoc-resource-fixtures/fixtures/resource.js
@@ -1,22 +1,82 @@
-/**
- * @class
- */
-class Resource {
+class Cart {
   /**
-   * @param {string} a
-   * @param {number} b
+   * A shopping cart for a user.
+   * @summary Creates a new cart for a user.
+   * @param {string} userId - The user's ID.
+   * @param {Array.<Object>} items - The items in the cart.
    */
-  constructor(a, b) {}
+  constructor(userId, items) {}
 
   /**
-   * @param {string} a
-   * @returns {boolean}
+   * Adds an item to the cart.
+   * @summary Puts a certain amount of an item in the cart.
+   * @param {string} itemId - The ID of the item to add.
+   * @param {number} quantity - How many of the item to add.
+   * @returns {boolean} - Returns true if the item is added successfully.
    */
-  method(a) {}
+  addItem(itemId, quantity) {}
+
+  /**
+   * Processes the items in the cart.
+   * @summary Handles the items in the cart.
+   * @param {Array.<number|string>} productIds - The IDs of the products.
+   * @param {Object[]} itemDetails - Information about the items.
+   * @param {Array.<number[]|string[]>} groupedItems - Grouped item info.
+   * @param {Array.<string>} itemNames - Names of the items.
+   * @param {Array.<number[]|number>} itemQuantities - Quantities of the items.
+   * @param {number[] | number | string[]} mixedValues - Other related values.
+   * @param {boolean} isActive - If the cart is active.
+   * @param {number} retryCount - How many times to try processing.
+   * @returns {boolean} - Returns true if processing is successful.
+   */
+  processCart(productIds, itemDetails, groupedItems, itemNames, itemQuantities, mixedValues, isActive, retryCount) {}
+}
+
+class Account {
+  /**
+   * A user account.
+   * @summary Creates a new account for a user.
+   * @param {string} username - The user's name.
+   * @param {number} balance - The account balance.
+   * @param {Array.<string>} transactionHistory - List of past transactions.
+   */
+  constructor(username, balance, transactionHistory) {}
+
+  /**
+   * Adds money to the account.
+   * @summary Deposits an amount into the account.
+   * @param {string} accountId - The ID of the account to deposit to.
+   * @param {number} depositAmount - How much to deposit.
+   * @returns {boolean} - Returns true if the deposit is successful.
+   */
+  deposit(accountId, depositAmount) {}
+
+  /**
+   * Checks transactions in the account.
+   * @summary Reviews the account's transactions.
+   * @param {string} accountId - The ID of the account to check.
+   * @param {Array.<number|string>} transactions - The transactions to review.
+   * @param {Object[]} transactionDetails - Details about each transaction.
+   * @param {Array.<number[]|string[]>} categorizedTransactions - Sorted transaction data.
+   * @returns {boolean} - Returns true if the check is successful.
+   */
+  auditTransactions(accountId, transactions, transactionDetails, categorizedTransactions) {}
 }
 
 /**
- * @param {string} a
- * @param {number} b
+ * Changes the stock for a product.
+ * @summary Updates the stock amount for a product.
+ * @param {string} productId - The ID of the product.
+ * @param {number} quantity - The new stock amount.
  */
-function foo(a, b) {}
+function updateStock(productId, quantity) {}
+
+/**
+ * Checks data for errors.
+ * @summary Validates data against certain rules.
+ * @param {Array.<Object>} data - The data to check.
+ * @param {Array.<number|string>} identifiers - IDs to check against.
+ * @param {number} maxLimit - The highest allowed limit.
+ * @returns {boolean} - Returns true if the data is okay.
+ */
+function validateData(data, identifiers, maxLimit) {}
