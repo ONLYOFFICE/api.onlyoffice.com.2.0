0e4a7df07544b1017dcd6e2ffd60d7f8c167368e
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-10-23 11:20:58 +0400

    add preact-template package

---
+++ packages/preact-template/lib/main.tsx
@@ -0,0 +1,14 @@
+import * as Elements from "@onlyoffice/preact-elements"
+import {type JSX, h} from "preact"
+import {renderToString} from "preact-render-to-string"
+
+export interface TemplateProperties extends Elements.TemplateProperties {
+  render?: typeof renderToString
+}
+
+export function Template(p: TemplateProperties): JSX.Element {
+  const {children, render = renderToString, ...o} = p
+  // @ts-ignore children will be resolved inside render function
+  o.dangerouslySetInnerHTML = {__html: render(children)}
+  return <Elements.Template {...o} />
+}
