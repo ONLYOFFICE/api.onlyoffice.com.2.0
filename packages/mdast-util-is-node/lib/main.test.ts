e94bd74e903a05a8d1887d737c6442d8207ab41c
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-11-07 16:16:13 +0400

    add mdast-util-is-node package

---
+++ packages/mdast-util-is-node/lib/main.test.ts
@@ -0,0 +1,60 @@
+import {test} from "uvu"
+import {is} from "uvu/assert"
+import {
+  isHeadingNode,
+  isHtmlNode,
+  isLiteralNode,
+  isNode,
+  isParagraphNode,
+  isParentNode,
+  isTextNode,
+} from "./main.ts"
+
+interface Is {
+  (u: unknown): boolean
+}
+
+const p0: [Is, unknown][] = [
+  [isNode, {type: ""}],
+  [isLiteralNode, {type: "", value: ""}],
+  [isParentNode, {type: "", children: []}],
+]
+
+for (const [xi, xn] of p0) {
+  test(`${xi.name}(): returns true for the '${JSON.stringify(xn)}'`, () => {
+    const a = xi(xn)
+    is(a, true)
+  })
+
+  test(`${xi.name}(): returns false for the '{}'`, () => {
+    const a = xi({})
+    is(a, false)
+  })
+}
+
+const p1: [Is, unknown][] = [
+  [isTextNode, {type: "text", value: ""}],
+  [isHtmlNode, {type: "html", value: ""}],
+  [isParagraphNode, {type: "paragraph", children: []}],
+  [isHeadingNode, {type: "heading", depth: 1, children: []}],
+]
+
+for (const [xi, xn] of p1) {
+  test(`${xi.name}(): returns true for the '${JSON.stringify(xn)}'`, () => {
+    const a = xi(xn)
+    is(a, true)
+  })
+
+  for (const [yi, yn] of p1) {
+    if (xi === yi) {
+      continue
+    }
+
+    test(`${xi.name}(): returns false for the '${JSON.stringify(yn)}'`, () => {
+      const a = xi(yn)
+      is(a, false)
+    })
+  }
+}
+
+test.run()
